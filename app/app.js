96935ed69901cd156abe4d67dfc371ebfa495412
NicoScarfiello <nramirez@fi.uba.ar>
2025-04-17 21:04:14 +0000

    Subo metricas con hotshots

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 import express from "express";
+import {
+  getStartTime,
+  registerResponseTime,
+  addVolumeForCurrency,
+  addNetVolume,
+  countSuccess,
+  countError
+} from "./metrics.js";
 
 import {
   init as exchangeInit,
@@ -20,10 +28,13 @@ app.use(express.json());
 // ACCOUNT endpoints
 
 app.get("/accounts", (req, res) => {
+  const start = getStartTime();
   res.json(getAccounts());
+  registerResponseTime("accounts_get_response_time", start);
 });
 
 app.put("/accounts/:id/balance", (req, res) => {
+  const start = getStartTime();
   const accountId = req.params.id;
   const { balance } = req.body;
 
@@ -33,16 +44,20 @@ app.put("/accounts/:id/balance", (req, res) => {
     setAccountBalance(accountId, balance);
 
     res.json(getAccounts());
+    registerResponseTime("accounts_put_response_time", start);
   }
 });
 
 // RATE endpoints
 
 app.get("/rates", (req, res) => {
+  const start = getStartTime();
   res.json(getRates());
+  registerResponseTime("rates_get_response_time", start);
 });
 
 app.put("/rates", (req, res) => {
+  const start = getStartTime();
   const { baseCurrency, counterCurrency, rate } = req.body;
 
   if (!baseCurrency || !counterCurrency || !rate) {
@@ -53,17 +68,21 @@ app.put("/rates", (req, res) => {
   setRate(newRateRequest);
 
   res.json(getRates());
+  registerResponseTime("rates_put_response_time", start);
 });
 
 // LOG endpoint
 
 app.get("/log", (req, res) => {
+  const start = getStartTime();
   res.json(getLog());
+  registerResponseTime("log_get_response_time", start);
 });
 
 // EXCHANGE endpoint
 
 app.post("/exchange", async (req, res) => {
+  const start = getStartTime();
   const {
     baseCurrency,
     counterCurrency,
@@ -79,15 +98,28 @@ app.post("/exchange", async (req, res) => {
     !counterAccountId ||
     !baseAmount
   ) {
+    countError("exchange");
+    registerResponseTime("exchange_post_response_time", start);
     return res.status(400).json({ error: "Malformed request" });
   }
 
   const exchangeRequest = { ...req.body };
   const exchangeResult = await exchange(exchangeRequest);
+  const counterAmount = exchangeResult.counterAmount;
+
+  addVolumeForCurrency(baseCurrency, baseAmount);
+  addVolumeForCurrency(counterCurrency, counterAmount);
+  
+  addNetVolume(baseCurrency, -baseAmount);
+  addNetVolume(counterCurrency, counterAmount);
 
   if (exchangeResult.ok) {
+    countSuccess("exchange");
+    registerResponseTime("exchange_post_response_time", start);
     res.status(200).json(exchangeResult);
   } else {
+    countError("exchange");
+    registerResponseTime("exchange_post_response_time", start);
     res.status(500).json(exchangeResult);
   }
 });
